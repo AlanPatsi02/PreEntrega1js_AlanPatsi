98e4f2c7987555f88c12fb8e0dc30f49df2d73d6
Alan Patsi <alanpatsi02@gmail.com>
2024-10-08 01:16:53 -0300

    Creamos la carpeta componets y el archivo NavBar y el respectivo código

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (
+    <nav>
+    <div className="brand">
+        <h2>Espacio-Gamer</h2>
+    </div>
+
+    <ul>
+        <li>Teclados</li>
+        <li>Monitores</li>
+        <li>Mouse</li>
+        <li>Auriculares</li>
+    </ul>
+
+    
+    </nav>
+    )
+}
+
+export default NavBar
